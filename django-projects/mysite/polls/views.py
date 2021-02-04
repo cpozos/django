1ee7aca05c6403d5b948ce821d21432e57e8d2e9
Carlos Alberto Pozos Villegas <cpozosvillegas@hotmail.com>
2021-02-03 21:41:34 -0600

    Adding html

--- django-projects/mysite/polls/views.py
+++ django-projects/mysite/polls/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You are at the polls index.")
+    return HttpResponse("Hello, world. b025cd6c You are at the polls index.")
